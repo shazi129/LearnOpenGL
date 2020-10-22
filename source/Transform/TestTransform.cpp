40a169ece1e727c1f8f4489423322c7b325f11a3
shazi129 <apply_count@sina.com>
2020-10-22 20:50:59 +0800

    add glm

---
+++ source/Transform/TestTransform.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+using namespace  std;
+
+void main()
+{
+	glm::mat4 trans(
+		1.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 0.0f
+	);
+	cout << trans[0][0] << trans[0][1] << endl;
+
+	system("pause");
+}
